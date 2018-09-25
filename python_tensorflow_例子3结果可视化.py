55d098c0edfeaefaa158dcecc1f7855dae43daf3
Rani <850150293@qq.com>
2018-09-25 12:00:34 +0800

    python tensorflow plot

--- python_tensorflow_例子3结果可视化.py
+++ python_tensorflow_例子3结果可视化.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
-    # add one more layer and return the output of this layer
     weights = tf.Variable(tf.random_normal([in_size, out_size]))
     biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
     wx_plus_b = tf.matmul(inputs, weights) + biases
@@ -18,22 +18,39 @@ x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
 noise = np.random.normal(0, 0.05, x_data.shape)
 y_data = np.square(x_data) - 0.5 + noise
 
+# begin structure
 xs = tf.placeholder(tf.float32, [None, 1])
 ys = tf.placeholder(tf.float32, [None, 1])
-
-l1 = add_layer(x_data, 1, 10, activation_function=tf.nn.relu)
+# hidden layer
+l1 = add_layer(xs, 1, 10, tf.nn.relu)
+# output layer
 prediction = add_layer(l1, 10, 1, activation_function=None)
 
-loss = tf.reduce_mean(tf.reduce_sum(tf.square(y_data-prediction), reduction_indices=[1]))
-
+# loss
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
+# train
 train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+# end structure
+# plat real data
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.scatter(x_data, y_data)
+plt.ion()  # 连续绘图，没有这一句的话plt.show()之后就绘制不上了
+plt.show()
 
-init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
 for i in range(1000):
     sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+    try:
+        ax.lines.remove(lines[0])
+    except Exception:
+        pass
+# 绘制结果
     if i % 50 == 0:
-        print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+        prediction_value = sess.run(prediction, feed_dict={xs: x_data})
+        lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
+        plt.pause(1)
 
